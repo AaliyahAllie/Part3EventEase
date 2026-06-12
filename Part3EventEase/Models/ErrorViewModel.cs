1c784b2acf07b199a6fee623e0e791acf91fbed0
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2026-06-12 12:38:44 +0200

    Add project files.

---
+++ Part3EventEase/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Part3EventEase.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
